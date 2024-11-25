7f043ae3b4f5d387f3465edcb6a57c7e85e58dca
Emilie Respingue <respingue@gmail.com>
2024-11-25 23:01:43 +0100

    Created first webservice
    Hello World works !

--- psychometrics-back/src/main/java/com/psychometrics/psychometrics_back/controllers/HelloController.java
+++ psychometrics-back/src/main/java/com/psychometrics/psychometrics_back/controllers/HelloController.java
@@ -1,4 +1,14 @@
 package com.psychometrics.psychometrics_back.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Coucou Darwi !";
+    }
 }
