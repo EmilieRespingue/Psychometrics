5bf3f720425795dd9f9377fd9b81243069bdc972
EmilieRespingue <146751299+EmilieRespingue@users.noreply.github.com>
2024-11-25 21:56:49 +0100

    Back creation (#33)

---
+++ psychometrics-back/src/test/java/com/psychometrics/psychometrics_back/PsychometricsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psychometrics.psychometrics_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychometricsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
