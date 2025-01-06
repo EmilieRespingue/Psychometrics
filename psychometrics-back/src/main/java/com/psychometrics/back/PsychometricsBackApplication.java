1982b8fb1db54c77d709086c66c175bb6c02d9bb
Emilie Respingue <respingue@gmail.com>
2025-01-06 21:13:39 +0100

    Hello controller - back

--- psychometrics-back/src/main/java/com/psychometrics/back/PsychometricsBackApplication.java
+++ psychometrics-back/src/main/java/com/psychometrics/back/PsychometricsBackApplication.java
@@ -1,4 +1,4 @@
-package com.psychometrics.psychometrics_back;
+package com.psychometrics.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ psychometrics-back/src/main/java/com/psychometrics/back/controllers/HelloController.java
@@ -0,0 +1,27 @@
+package com.psychometrics.back.controllers;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Coucou Darwi !";
+    }
+
+    // Exemple d'un endpoint JSON
+    @GetMapping("/message")
+    public ResponseEntity<Map<String, String>> getMessage() {
+        Map<String, String> message = new HashMap<>();
+        message.put("content", "Bonjour depuis Spring Boot!");
+        return ResponseEntity.ok(message);
+    }
+}
