ba3336a3cb3f720b5048ed64c226edba9e9787ce
Emilie Respingue <respingue@gmail.com>
2025-01-07 22:59:33 +0100

    Feat: integrate routing and update app component structure. Add global styles and primary button styling.

--- psychometrics-front/src/app/app.component.ts
+++ psychometrics-front/src/app/app.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { HelloComponent } from '../components/hello/hello.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HelloComponent],
+  imports: [HelloComponent, RouterModule],
   template: `
     <div>
       <h1>Psychometrics</h1>
-      <app-hello></app-hello>
+      <router-outlet></router-outlet>
+      <section>
+        <app-hello></app-hello>
+      </section>
     </div>
   `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'psychometrics-front';
+  title = 'Psychometrics';
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle(this.title);
+  }
 }
