5a48756b97e192cbaf47c59224d397b2d3d798dc
Emilie Respingue <respingue@gmail.com>
2025-01-30 16:42:03 +0100

    Ajout du scope transloco dans la page info

--- psychometrics-front/src/app/pages/info/info.component.ts
+++ psychometrics-front/src/app/pages/info/info.component.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [],
-  template: ` <p>info works!</p> `,
+  imports: [TranslocoModule],
+  template: `
+    <div class="info-container">
+      <p>{{ 'info.info' | transloco }}</p>
+    </div>
+  `,
   styleUrl: './info.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
