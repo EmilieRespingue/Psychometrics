6619f5760edc5cb6bf8640dd249cb5eb81668c82
Emilie Respingue <respingue@gmail.com>
2025-01-31 22:58:29 +0100

    Feat: Create login button and add icon in the header

--- psychometrics-front/src/app/components/header/header.component.ts
+++ psychometrics-front/src/app/components/header/header.component.ts
@@ -1,15 +1,25 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 import { TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   template: `
     <header class="header">
       <nav class="nav">
-        <button class="lang-btn" (click)="switchLang('fr')">FR</button>
-        <button class="lang-btn" (click)="switchLang('en')">EN</button>
+        <div class="lang-buttons">
+          <button class="lang-btn" (click)="switchLang('fr')">FR</button>
+          <button class="lang-btn" (click)="switchLang('en')">EN</button>
+        </div>
+
+        <button mat-button class="login-btn" (click)="navigateToLogin()">
+          <mat-icon>person</mat-icon>
+          <span class="login-text">Se connecter /<br />S'inscrire</span>
+        </button>
       </nav>
     </header>
   `,
@@ -18,8 +28,15 @@ import { TranslocoService } from '@jsverse/transloco';
 })
 export class HeaderComponent {
   private translocoService = inject(TranslocoService);
+  private router = inject(Router);
 
   switchLang(lang: 'fr' | 'en') {
     this.translocoService.setActiveLang(lang);
+  }
+
+  navigateToLogin() {
+    //Pour l'instant on log juste, à implémenter plus tard
+    console.log('Navigation vers la page de connexion');
+    //this.router.navigate(['/login]);
   }
 }
