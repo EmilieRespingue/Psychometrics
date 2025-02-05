3940661697f913891ac568830f07bb6df98584a1
GargamelLeNoir <zegargamel@hotmail.com>
2025-02-05 22:16:21 +0100

    Initial database link

--- psychometrics-back/src/main/java/com/psychometrics/back/controllers/HelloController.java
+++ psychometrics-back/src/main/java/com/psychometrics/back/controllers/HelloController.java
@@ -1,17 +1,22 @@
 package com.psychometrics.back.controllers;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class HelloController {
 
+	@Autowired
+	DataSource dataSource;
     @GetMapping("/coucou")
     public ResponseEntity<Map<String, String>> sayHello() {
         Map<String, String> message = new HashMap<>();
@@ -23,7 +28,8 @@ public class HelloController {
     @GetMapping("/message")
     public ResponseEntity<Map<String, String>> getMessage() {
         Map<String, String> message = new HashMap<>();
-        message.put("content", "Coucou Garga !");
+		message.put("content", "Coucou Gargax !");
+		System.out.println("@@DATA SOURCE 2 " + dataSource);
         return ResponseEntity.ok(message);
     }
 }
